ee0c6563301e6f1b76a3a3a5408b62936ba36a78
segur <segur.opus@gmail.com>
2020-12-16 18:54:55 +0900

    Add navbar

--- docs/main.js
+++ docs/main.js
@@ -3,31 +3,9 @@ const sendVideoPlayingToUnity = (isPlaying) => {
     if (window.vuplex) {
         window.vuplex.postMessage(isPlaying);
     }
-}
+};
 
-const toggleVideo = (isPlaying) => {
-    const v = document.getElementById('video');
-    const state = document.getElementById('state');
-    if (isPlaying) {
-        v.play();
-        state.textContent = '再生中';
-    } else {
-        v.pause();
-        state.textContent = '停止中';
-    }
-}
-
-const addMessageListenerFromUnity = () => {
-    window.vuplex.addEventListener('message', (event) => {
-        const json = event.data;
-        console.log('JSON received: ' + json);
-        if (json.target === "video") {
-            toggleVideo(json.play === "true");
-        }
-    });
-}
-
-document.addEventListener('DOMContentLoaded', () => {
+const addEventListenerForVideo = () => {
     const v = document.getElementById('video');
     const state = document.getElementById('state');
 
@@ -39,6 +17,32 @@ document.addEventListener('DOMContentLoaded', () => {
         state.textContent = '停止中';
         sendVideoPlayingToUnity(false);
     })
+};
+
+const toggleVideo = (isPlaying) => {
+    const v = document.getElementById('video');
+    const state = document.getElementById('state');
+    if (isPlaying) {
+        v.play();
+        state.textContent = '再生中';
+    } else {
+        v.pause();
+        state.textContent = '停止中';
+    }
+};
+
+const addMessageListenerFromUnity = () => {
+    window.vuplex.addEventListener('message', (event) => {
+        const json = event.data;
+        console.log('JSON received: ' + json);
+        if (json.target === "video") {
+            toggleVideo(json.play === "true");
+        }
+    });
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+    addEventListenerForVideo();
 
     if (window.vuplex) {
         addMessageListenerFromUnity();
